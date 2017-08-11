f3a5363f62a29454a4435c0e7facc1f6fe263054
MarioRodeghiero <mario.rodeghiero@gmail.com>
2017-08-10 23:06:30 -0300

    iniciado curso avançado de javaScript

---
+++ aluraframe/client/js/index.js
@@ -0,0 +1,14 @@
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#quantidade'),
+    document.querySelector('#valor')
+];
+
+console.log('------------------------------------');
+console.log(campos);
+console.log('------------------------------------');
+
+document.querySelector('.form').addEventListener('submit', function(event){
+
+    alert("oi");
+});
