2862fca5a6617508b7be58dbf9c9c1a2523e30ce
MarioRodeghiero <mario.rodeghiero@gmail.com>
2017-09-18 23:43:52 -0300

    start class 4

--- aluraframe/client/js/app/controller/negociacao-controller.js
+++ aluraframe/client/js/app/controller/negociacao-controller.js
@@ -1,15 +1,28 @@
 class NegociacaoController {
 
-    adiciona(event) {
-        event.preventDefault();
-        
-        let $ = document.querySelector.bind(document);
-        let inputData= $('#data');
-        let inputQuantidade = $('#quantidade');
-        let inputValor = $('#valor');
+    constructor(){
 
-        console.log(inputData.value);
-        console.log(inputQuantidade.value);
-        console.log(inputValor.value);
+        let $ = document.querySelector.bind(document);
+        this._inputData= $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+
+    adiciona(event) {
+        
+        event.preventDefault();
+
+        let data = new Date(this._inputData.value);
+            // .split('-')
+            // .map((item, indice) => item - indice % 2)
+        // );
+        
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+          );
+        
+          console.log(negociacao);
     }
 }
