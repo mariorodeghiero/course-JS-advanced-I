b9acbbbed7200eac65112c0c202772c029965249
MarioRodeghiero <mario.rodeghiero@gmail.com>
2017-09-19 22:25:14 -0300

    work with date

--- aluraframe/client/js/app/controller/negociacao-controller.js
+++ aluraframe/client/js/app/controller/negociacao-controller.js
@@ -1,28 +1,23 @@
 class NegociacaoController {
 
-    constructor(){
+    constructor() {
 
         let $ = document.querySelector.bind(document);
-        this._inputData= $('#data');
+        this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
     }
 
     adiciona(event) {
-        
+
         event.preventDefault();
 
-        let data = new Date(this._inputData.value);
-            // .split('-')
-            // .map((item, indice) => item - indice % 2)
-        // );
-        
-        let negociacao = new Negociacao(
-            data,
-            this._inputQuantidade.value,
-            this._inputValor.value
-          );
-        
-          console.log(negociacao);
+        let helper = new DateHelper();
+
+        let negociacao = new Negociacao {
+            helper.textoParaData(this._inputData.value),
+                this._inputQuantidade.value,
+                this._inputValor.valor
+        };
     }
 }
